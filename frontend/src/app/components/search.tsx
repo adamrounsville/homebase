2e310699462a50ce38085ec777bfa568d6faab90
Freddy Rios <freddyrios2000@gmail.com>
2024-10-18 19:35:41 -0600

    Test Branch

--- frontend/src/app/components/search.tsx
+++ frontend/src/app/components/search.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
  
 function SearchBar() {
+  // test
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
       <Input type="search" placeholder="Enter a Location" className="homebase-input"/>
